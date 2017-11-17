a7eaec630e1c88143897b8493f7ea454441b4a3e
Lc <apple@0587378238.wireless.umich.net>
2017-11-16 21:07:43 -0500

    upload video works

--- auteur/main.py
+++ auteur/main.py
@@ -1,12 +1,14 @@
 #Imports
-from flask import Flask, render_template #import Flask, Jinja2 rendering
+from flask import Flask, render_template, request, redirect, url_for #import Flask, Jinja2 rendering
 from flask_bootstrap import Bootstrap #import bootstrap - don't forget to pip install flask-bootstrap first
 from flask_script import Manager #import flask-script
 from flask_sqlalchemy import SQLAlchemy
+from flask import flash
 import os
 from flask_wtf import Form
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required
+from datetime import datetime
 
 #############################
 ##          Config         ##
@@ -50,18 +52,54 @@ db = SQLAlchemy(app)
 #             name=form.name.data
 #             db.session.commit()
 #     return render_template('index.html', form=form, name=name)
+#Put the class in the front
+
+#Added an index page
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/project')
+def project():
+    videos= Video.query.order_by(Video.date.desc()).all()
+    return render_template('project.html',videos=videos)
+#app.route('/project/<int_project_id>/<title>')
+@app.route('/video/<video_title>')
+def video(video_title):
+    video= Video.query.filter_by(title=video_title).one()
+    date=video.date.strftime('%B %d, %Y ')
+    videos= Video.query.order_by(Video.date.desc()).all()
+    return render_template('video.html',video=video,date=date,videos=videos)
+
+@app.route('/upload')
 def upload():
-    form = UploadForm()
-    if form.validate_on_submit():
-        video = Video(title=form.title.data,
-                    description=form.description.data,
-                    project_id=form.project_id.data)
-        db.session.add(video)
-    return render_template('upload.html', form=form)
+    return render_template('upload.html')
+
+@app.route('/create_db')
+def create_db():
+    db.drop_all()
+    db.create_all()
+    return '<h1>Database Created</h1>'
+#There is a separate route to handle the upload
+@app.route('/uploadvideo', methods=['POST'])
+def uploadvideo():
+    title=request.form['title']
+    description=request.form['description']
+    video_file=request.files['inputFile']
+    video = Video(title=title,description=description,date=datetime.now(),video_file=video_file.read())
+    db.session.add(video)
+    db.session.commit()
+    flash('You have uploaded a video sucessfully')
+    return redirect(url_for('index'))
+    #form = UploadForm()
+    #if form.validate_on_submit():
+        #video = Video(title=form.title.data,
+                    #description=form.description.data,
+                    #project_id=form.project_id.data)
+        #db.session.add(video)
+    #return render_template('upload.html', form=form)
 
 # @app.route('/<name>')
 # def user(name):
@@ -71,10 +109,6 @@ def upload():
 #     else:
 #         return 'This user is %s' % existinguser.ready
 
-@app.route('/create_db')
-def create_db():
-    db.create_all()
-    return '<h1>Database Created</h1>'
 
 #############################
 ## Model Definitions Below ##
@@ -96,6 +130,9 @@ class UploadForm(Form):
     #     if User.query.filter_by(email=field.data).first():
     #         raise ValidationError('Email taken')
 
+
+#route for creating a new database
+
 class Video(db.Model):
     __tablename__ = 'videos'
     id = db.Column(db.Integer, primary_key=True)
@@ -105,7 +142,7 @@ class Video(db.Model):
     comments = db.Column(db.String(64), unique=True, index=True)
     description = db.Column(db.String(64), nullable=True)
     likes = db.Column(db.Integer)
-    video = db.Column(db.LargeBinary) # what should be video datatype be?????
+    video_file = db.Column(db.LargeBinary) # what should be video datatype be?????
     date = db.Column(db.DateTime) # not in scope right now
 
 class User(db.Model): # what is UserMixin???
@@ -113,9 +150,28 @@ class User(db.Model): # what is UserMixin???
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True)
 
+class Project(db.Model):
+    __tablename__ = 'projects'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    name = db.Column(db.String(64))
+    date = db.Column(db.DateTime)
+    genre = db.Column(db.String(64)) ##or should we use Integer
+    description = db.Column(db.String(64))
+    picture = db.Column(db.LargeBinary)
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    video_id = db.Column(db.Integer)
+    content = db.Column(db.String(64))
+    time = db.Column(db.Integer) ###how to implement this?
+
 
 ###########################################
 ## Run the Flask Webserver in debut mode ##
 ###########################################
-if __name__=='__main__': #only do the following if the script is executed directly skip if imported
+if __name__=='__main__':#only do the following if the script is executed directly skip if imported
+    app.config['TEMPLATES_AUTO_RELOAD']=True
     app.run(debug=True) #start the integrated flask webserver
