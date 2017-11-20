3c12479e49b0e81c48648a936db4417d0996b253
kumajingshu <zhaojingshu@yahoo.com>
2017-11-20 11:29:32 -0500

    updates

--- auteur/main.py
+++ auteur/main.py
@@ -1,5 +1,5 @@
 #Imports
-from flask import Flask, render_template, request, redirect, url_for #import Flask, Jinja2 rendering
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory #import Flask, Jinja2 rendering
 from flask_bootstrap import Bootstrap #import bootstrap - don't forget to pip install flask-bootstrap first
 from flask_script import Manager #import flask-script
 from flask_sqlalchemy import SQLAlchemy
@@ -14,7 +14,7 @@ from datetime import datetime
 #############################
 ##          Config         ##
 #############################
-reload(sys)  
+reload(sys)
 sys.setdefaultencoding('latin-1')
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__) #Pass the __name__ argument to the Flask application constructor
@@ -25,50 +25,30 @@ app.config['SECRET_KEY'] = 'you_should_really_have_this_be_an_environment_variab
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
-#############################
-## Form Definitions Below ##
-#############################
+#Video Upload Config
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# class NameForm(Form):
-#     name = StringField('What would you like me to call you?', validators=[Required()])
-#     ready = SelectField("Are you ready for class?",choices=[('ready', 'So. Ready.'), ('sleeping', 'So. Tired.'), ('busy', 'So. Busy.')])
-#     submit = SubmitField('Submit')
+
 
 #############################
 ## Route Definitions Below ##
 #############################
 
-# @app.route('/', methods=['GET', 'POST']) #define the route for <server>/
-# def index(): #index function
-#     name = None
-#     form = NameForm()
-#     if form.validate_on_submit():
-#         existinguser=User.query.filter_by(username=form.name.data).first()
-#         if existinguser is None:
-#             name=form.name.data
-#             user=User(username=form.name.data, ready=form.ready.data)
-#             db.session.add(user)
-#         else:
-#             existinguser.username=form.name.data
-#             existinguser.ready=form.ready.data
-#             name=form.name.data
-#             db.session.commit()
-#     return render_template('index.html', form=form, name=name)
-#Put the class in the front
-
-#Added an index page
-
-
+#Landing Page, Playback Multiple Videos
 @app.route('/')
 def index():
+    projects=Project.query.order_by(Project.date.desc()).all()
     videos= Video.query.order_by(Video.date.desc()).all()
-    return render_template('index.html',videos=videos)
+    return render_template('index.html',videos=videos,projects=projects)
 
-@app.route('/project')
-def project():
+#Project Page
+@app.route('/project/<project_name>')
+def project(project_name):
+    project=Project.query.filter_by(name=project_name).one()
     videos= Video.query.order_by(Video.date.desc()).all()
-    return render_template('project.html',videos=videos)
-#app.route('/project/<int_project_id>/<title>')
+    return render_template('project.html',project=project,videos=videos)
+
+#Video Page
 @app.route('/video/<video_title>')
 def video(video_title):
     video= Video.query.filter_by(title=video_title).one()
@@ -76,65 +56,69 @@ def video(video_title):
     videos= Video.query.order_by(Video.date.desc()).all()
     return render_template('video.html',video=video,date=date,videos=videos)
 
+#Create Project Page
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+#Upload Video Page
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
 
+#Create Database
 @app.route('/create_db')
 def create_db():
     db.drop_all()
     db.create_all()
     return '<h1>Database Created</h1>'
-#There is a separate route to handle the upload
+#Create Project -- Post
+@app.route('/createproject', methods=['POST'])
+def createproject():
+
+    #Image Upload Store File
+    target = os.path.join(APP_ROOT, 'static/img/')
+    for file in request.files.getlist("file"):
+        image_file=file.filename
+        destination = "/".join([target, image_file])
+        file.save(destination)
+
+    name=request.form['name']
+    project_description=request.form['project_description']
+    # video_file=request.files['inputFile']
+    videos= Video.query.order_by(Video.date.desc()).all()
+    project=Project(picture=image_file,name=name,description=project_description)
+    db.session.add(project)
+    db.session.commit()
+    #flash('You have uploaded a video sucessfully')
+    return render_template('project.html', project=project,videos=videos)
+
+#Upload Video -- Post
 @app.route('/uploadvideo', methods=['POST'])
 def uploadvideo():
+
+    #Video Upload Store File
+    target = os.path.join(APP_ROOT, 'static/vid/')
+    for file in request.files.getlist("file"):
+        video_file=file.filename
+        destination = "/".join([target, video_file])
+        file.save(destination)
+
     title=request.form['title']
     description=request.form['description']
-    video_file=request.files['inputFile']
-    video = Video(title=title,description=description,date=datetime.now(),video_file=video_file.read())
+    video = Video(vid_name=video_file,title=title,description=description,date=datetime.now())
     db.session.add(video)
     db.session.commit()
-    flash('You have uploaded a video sucessfully')
-    return redirect(url_for('index'))
-    #form = UploadForm()
-    #if form.validate_on_submit():
-        #video = Video(title=form.title.data,
-                    #description=form.description.data,
-                    #project_id=form.project_id.data)
-        #db.session.add(video)
-    #return render_template('upload.html', form=form)
+    return render_template('video.html', video=video)
 
-# @app.route('/<name>')
-# def user(name):
-#     existinguser=User.query.filter_by(username=name).first()
-#     if existinguser is None:
-#         return 'I do not know who this is.'
-#     else:
-#         return 'This user is %s' % existinguser.ready
-
+#Playback Single Video
+# @app.route('/../static/vid/<filename>')
+# def send_vid(filename):
+#     return send_from_directory("vid", filename)
 
 #############################
 ## Model Definitions Below ##
 #############################
-class UploadForm(Form):
-    pass
-    # email = StringField('Email', validators=[Required(), Length(1, 64),
-    #                                        Email()])
-    # username = StringField('Username', validators=[
-    #     Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-    #                                       'Usernames must have only letters, '
-    #                                       'numbers, dots or underscores')])
-    # password = PasswordField('Password', validators=[
-    #     Required(), EqualTo('password2', message='Passwords must match.')])
-    # password2 = PasswordField('Confirm password', validators=[Required()])
-    # submit = SubmitField('Register')
-    #
-    # def validate_email(self, field):
-    #     if User.query.filter_by(email=field.data).first():
-    #         raise ValidationError('Email taken')
-
-
-#route for creating a new database
 
 class Video(db.Model):
     __tablename__ = 'videos'
@@ -145,7 +129,7 @@ class Video(db.Model):
     comments = db.Column(db.String(64), unique=True, index=True)
     description = db.Column(db.String(64), nullable=True)
     likes = db.Column(db.Integer)
-    video_file = db.Column(db.LargeBinary) # what should be video datatype be?????
+    vid_name = db.Column(db.String, default=None, nullable=True)
     date = db.Column(db.DateTime) # not in scope right now
 
 class User(db.Model): # what is UserMixin???
@@ -175,6 +159,6 @@ class Comment(db.Model):
 ###########################################
 ## Run the Flask Webserver in debut mode ##
 ###########################################
-if __name__=='__main__':#only do the following if the script is executed directly skip if imported
+if __name__=='__main__':
     app.config['TEMPLATES_AUTO_RELOAD']=True
-    app.run(debug=True) #start the integrated flask webserver
+    app.run(debug=True) 
